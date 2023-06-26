d8e03a043418cc28a7e8c9bc0b97d16e7ac76df3
karabas <gustov.andrei@mail.ru>
2023-06-26 19:43:31 +0300

    005 условие

---
+++ EX005_Name/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите имя ползователя:");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.Write("Ура, Маша пришла");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);   
+}
